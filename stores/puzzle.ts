bf0db440a63f0aeec685133ed066a3851b1d5dd8
Antoine Delahaye <46669439+antoine-delahaye@users.noreply.github.com>
2022-09-05 22:45:24 +0200

    Populate puzzles data

--- stores/puzzle.ts
+++ stores/puzzle.ts
@@ -11,7 +11,60 @@ export const puzzleStore = defineStore("puzzle", () => {
       }
     },
     theSequence: {
-      clues: 4
+      clues: 4,
+      size: {
+        i: 4,
+        j: 4
+      }
+    },
+    suchDogSuchMaster: {
+      clues: 5,
+      size: {
+        i: 3,
+        j: 5
+      }
+    },
+    augustInFire: {
+      clues: 4,
+      size: {
+        i: 3,
+        j: 5
+      }
+    },
+    homeMade: {
+      clues: 5,
+      size: {
+        i: 3,
+        j: 5
+      }
+    },
+    dollHouse: {
+      clues: 4,
+      size: {
+        i: 3,
+        j: 5
+      }
+    },
+    toWantAndToBeAbleTo: {
+      clues: 6,
+      size: {
+        i: 3,
+        j: 5
+      }
+    },
+    checkmate: {
+      clues: 4,
+      size: {
+        i: 3,
+        j: 5
+      }
+    },
+    hereWeGoAgain: {
+      clues: 5,
+      size: {
+        i: 4,
+        j: 4
+      }
     }
   })
 
